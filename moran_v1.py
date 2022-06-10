7d09aa5504183cf3a7d101e39f540798348ce020
Paulameena Shultes <32913103+paulameena@users.noreply.github.com>
2022-06-10 12:19:40 -0400

    simplifying first try after second paper read

--- moran_v1.py
+++ moran_v1.py
@@ -17,42 +17,16 @@ def run_moran_process(N, r, num_muts, pop_type):
     return fix_prob, fix_time
 
 def prob_fixation(num_muts,r, N, pop_type):
-    return
+    if pop_type == "well_mixed" or "ring":
+        return prob_fixation_well_mixed(num_muts, r, N)
+    else:
+        return prob_fixation_other(num_muts, r, N)
 
 def prob_fixation_well_mixed(num_muts, r, N):
     return (1 - (1/r**num_muts))/(1-(1/r**N))
 
+def prob_fixation_other(num_muts, r, N):
+   return (1 - (1/r**(2*num_muts)))/(1-(1/r**(2*N))) 
+
 def ave_time_to_fixation():
     return 
-
-def t_plus_well_mixed(r, num_muts, N):
-    return (r*num_muts)/(r*num_muts + N - num_muts) * (N-num_muts)/N
-
-def t_minus_well_mixed(r, num_muts, N):
-    return (num_muts)/(r*num_muts + N - num_muts) * (N-num_muts)/N 
-
-def pi_fxn_prob(limit, pop_type):
-    reserve = 1
-    j = 1
-    if pop_type == "well-mixed":
-        while j <= limit:
-            reserve *= t_minus_well_mixed(j)/t_plus_well_mixed(j)
-            j += 1
-        return reserve
-    elif pop_type == "ring":
-        while j <= limit:
-            reserve *= 
-            j += 1
-        return reserve 
-    elif pop_type == "star":
-        while j <= limit:
-            reserve *= 
-            j += 1
-        return reserve 
-
-
-def sigma_fxn_prob(limit, pop_type):
-    reserve = 0
-    k = 1
-    while k <= limit:
-        reserve += pi_fxn_prob(k, pop_type)
