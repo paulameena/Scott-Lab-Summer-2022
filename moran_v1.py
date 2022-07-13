533c59ce177e82e9777f19cd55f51e1e96fce319
Paulameena Shultes <32913103+paulameena@users.noreply.github.com>
2022-07-13 15:51:56 -0400

    testing'

--- moran_v1.py
+++ moran_v1.py
@@ -1,12 +1,6 @@
 
-from appscript import k
-import matplotlib as mpl 
+import matplotlib.pyplot as plt
 import numpy as np
-import sys
-import os
-import pandas as pd
-import test
-from scipy import special, optimize, integrate, stats
 
 def moran_process(num_muts, r, N, pop_type): 
     ## N is the population size ,
@@ -24,24 +18,52 @@ def moran_process(num_muts, r, N, pop_type):
     else:
         Tpos_func = 0
         Tneg_func = 0
-    fix_prob, fix_time = np.array(), np.array()
-    for i in range(num_muts, N):
-        fix_prob.append(fix_prob(i, N, Tneg_func, Tpos_func))
-        fix_time.apend(fix-time(i, N, Tneg_func, Tpos_func))
+    fix_prob, fix_time = fix_prob_func(1, N, Tneg_func, Tpos_func), fix_time_func(1, N, Tneg_func, Tpos_func)
+    #for i in range(num_muts, N):
+       # fix_prob.append(fix_prob_func(i, N, Tneg_func, Tpos_func))
+        #fix_time.append(fix_time_func(i, N, Tneg_func, Tpos_func))
     return fix_prob, fix_time
 
-def fix_prob(l, N, Tneg_func, Tpos_func):
+def fix_prob_func(l, N, Tneg_func, Tpos_func):
     first = lambda j,k: np.product([Tneg_func(x)/Tpos_func(x) for x in range(j,k+1)])
     second = lambda k, top: np.sum([first(k, z) for z in range(k, top)])
     numerator = 1 + second(1, l)
     denominator = 1 + second(1, N)
     return numerator/denominator 
 
-def fix_time(l, N, Tneg_func, Tpos_func):
+def fix_time_func(l, N, Tneg_func, Tpos_func):
     first = lambda m, k: np.product([Tneg_func(x)/Tpos_func(x) for x in range(m, k+1)])
-    second = lambda l,k: np.sum([first(l+1, y) * fix_prob(y, N, Tneg_func, Tpos_func)/Tpos_func(y) for y in range(l,k+1)])
+    second = lambda l,k: np.sum([first(l+1, y) * fix_prob_func(y, N, Tneg_func, Tpos_func)/Tpos_func(y) for y in range(l,k+1)])
     third = lambda k,top: np.sum([second(k, z) for z in range(k, top)])
     return third(l, N)
 
-moran_process(1, 1.1, 50, "well-mixed")
+def simulate_moran_process(start_adv, final_adv, increment, N, pop_type):
+    fix_prob_array = []
+    fix_time_array = []
+    adv_array = []
+    for r in np.arange(start_adv, final_adv, increment):
+        adv_array.append(r)
+        intermed = moran_process(1, r, N, pop_type)
+        fix_prob_array.append(intermed[0])
+        fix_time_array.append(intermed[1])
+    return adv_array, fix_prob_array, fix_time_array
+
+def plot_moran_process(advantages, fix_probs, fix_times):
+    figure, axis = plt.subplots(1,2)
+    axis[0].plot(advantages, fix_probs)
+    axis[0].set_title("Fixation Probabilities vs Selective Advantage of Mutant")
+    axis[1].plot(advantages, fix_times)
+    axis[1].set_title("Fixation Times vs Selective Advantage of Mutant")
+    plt.show()
+
+
+def run():
+    advs, probs, times = simulate_moran_process(1, 5.0, 0.2, 50, "well-mixed")
+    plot_moran_process(advs, probs, times)
+
+run()
+
+
+
+
 
